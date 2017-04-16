a67191c9f07ad446c0f2c1776105ff0f8c0d4b55
SEO <malacma@gmail.com>
2017-04-16 00:50:55 -0300

    Plugin facebook + API working

--- www/js/index.js
+++ www/js/index.js
@@ -17,61 +17,77 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function () {
+// Application Constructor
+    initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
-
     // deviceready Event Handler
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
-    onDeviceReady: function () {
+    onDeviceReady: function() {
         this.receivedEvent('deviceready');
-        document.getElementById('btn-submit').onclick = function () {
+        document.getElementById('btn-submit').onclick = function() {
             document.getElementById('page1').style.display = 'block';
             document.getElementById('page-signin').style.display = 'none';
         }
     },
-
     // Update DOM on a Received Event
-    receivedEvent: function (id) {
+    receivedEvent: function(id) {
         /*var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+         var listeningElement = parentElement.querySelector('.listening');
+         var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+         listeningElement.setAttribute('style', 'display:none;');
+         receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);*/
+         console.log('Received Event: ' + id);*/
     }
 };
-
 app.initialize();
-
-
 function loginGoogle() {
     window.plugins.googleplus.login(
-        {
-            scopes: 'profile email', // optional, space-separated(!) list of scopes, If not included or empty, defaults to 'profile email'.
-            webClientId: '45315661768-umuoma1q6a4ghk9eodn5rvb34c2tsl8e.apps.googleusercontent.com', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
-            offline: true // optional, but requires the webClientId - if set to true the plugin will also return a serverAuthCode, which can be used to grant offline access to a non-Google server
-        },
-        function (obj) {
-            alert(JSON.stringify(obj)); // do something useful instead of alerting
-            /* contains:
-             obj.email          // 'eddyverbruggen@gmail.com'
-             obj.userId         // user id
-             obj.displayName    // 'Eddy Verbruggen'
-             obj.familyName     // 'Verbruggen'
-             obj.givenName      // 'Eddy'
-             obj.imageUrl       // 'http://link-to-my-profilepic.google.com'
-             obj.idToken        // idToken that can be exchanged to verify user identity.
-             obj.serverAuthCode // Auth code that can be exchanged for an access token and refresh token for offline access
-            */
-        },
-        function (msg) {
-            alert('error: ' + msg);
-        }
+            {
+                scopes: 'profile email', // optional, space-separated(!) list of scopes, If not included or empty, defaults to 'profile email'.
+                webClientId: '302835253273-fqb091vas7jtm62h1cbi8slib42brtpr.apps.googleusercontent.com', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
+                offline: true // optional, but requires the webClientId - if set to true the plugin will also return a serverAuthCode, which can be used to grant offline access to a non-Google server
+            },
+    function(obj) {
+        alert(JSON.stringify(obj)); // do something useful instead of alerting
+        /* contains:
+         obj.email          // 'eddyverbruggen@gmail.com'
+         obj.userId         // user id
+         obj.displayName    // 'Eddy Verbruggen'
+         obj.familyName     // 'Verbruggen'
+         obj.givenName      // 'Eddy'
+         obj.imageUrl       // 'http://link-to-my-profilepic.google.com'
+         obj.idToken        // idToken that can be exchanged to verify user identity.
+         obj.serverAuthCode // Auth code that can be exchanged for an access token and refresh token for offline access
+         */
+    },
+            function(msg) {
+                alert('error: ' + msg);
+            }
     );
+}
+
+function loginFacebook() {
+    var fbLoginSuccess = function(userData) {
+        alert("UserInfo: " + JSON.stringify(userData));
+        //me?fields=id,name,email
+        facebookConnectPlugin.api(
+                "me/?fields=id,name,email,picture", // graph path
+                [], // array of additional permissions
+                function(response) {
+                    if (response.error) {
+                        alert("Uh-oh! " + JSON.stringify(response.error));
+                    } else {
+                        alert(JSON.stringify(response));
+                    }
+                });
+    }
+
+    facebookConnectPlugin.login(["public_profile", "email"], fbLoginSuccess, function(error) {
+        alert("" + error)
+    });
 }
